5e1f4ecce0e422741f199a616176b1bd00ef0596
Mauricio Yukio Tonooka <mytonooka@MacBook-Air-de-Mauricio.local>
2024-03-15 15:25:59 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/mytonooka/course/entites/User.java
+++ src/main/java/com/mytonooka/course/entites/User.java
@@ -1,20 +1,23 @@
 package com.mytonooka.course.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
-	
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,9 +25,12 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -35,7 +41,6 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	
 	public Long getId() {
 		return id;
 	}
@@ -76,7 +81,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -92,6 +100,6 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+
 	}
-	
 }
