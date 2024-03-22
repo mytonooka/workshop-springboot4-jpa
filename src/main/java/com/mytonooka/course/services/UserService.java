f6c6729f59d630d013e587c7c8691c318849fa33
mytonooka <mytonooka@yahoo.com.br>
2024-03-21 22:09:37 -0300

    Exception handling - update

--- src/main/java/com/mytonooka/course/services/UserService.java
+++ src/main/java/com/mytonooka/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.mytonooka.course.repositories.UserRepository;
 import com.mytonooka.course.services.exceptions.DatabaseException;
 import com.mytonooka.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -52,9 +54,18 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		
+		try {
+		
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException(id);
+		
+		}
 	}
 
 	private void updateData(User entity, User obj) {
